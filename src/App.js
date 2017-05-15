3f32d2edff19ca83c0bced5a83505b0c5e8687fe
Baptiste Jouk <baptistejouk@macbook-pro-de-baptiste.home>
2017-05-15 15:14:08 +0200

    Added full tab support

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {Nav, NavItem,ListGroup,ListGroupItem, Button,Glyphicon,InputGroup,FormCo
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={count:2,number_tabs:2, active_tab:'0',tabs_titles:['Main','Annex'],tasks:[{title:'Hi',complete:false,tab:0,id:0},{title:'Hiii',complete:false,tab:1,id:1}]}
+    this.state={count:2,number_tabs:2, active_tab:'0',tabs_titles:[{title:'Main',id:0,editable:false},{title:'Annex',id:1,editable:false}],tasks:[{title:'Hi',complete:false,tab:0,id:0},{title:'Hiii',complete:false,tab:1,id:1}]}
     this.changeTab=this.changeTab.bind(this);
     this.addTab=this.addTab.bind(this);
     this.toggleTask=this.toggleTask.bind(this);
@@ -16,25 +16,46 @@ class App extends Component {
     this.deleteTask=this.deleteTask.bind(this)
     this.removeTab=this.removeTab.bind(this)
     this.editTab=this.editTab.bind(this)
+    this.handleTabChange=this.handleTabChange.bind(this)
+    this.validateTabChange=this.validateTabChange.bind(this)
   }
   removeTab(e){
     var arr=this.state.tabs_titles.slice(0)
-    console.log(e.target.id,arr)
-    arr.splice(e.target.id,1)
+    console.log(e.target.id)
+    arr=arr.filter(function(ele){return ele.id!=e.target.id})
     console.log(arr)
-    this.setState({count:this.state.count,number_tabs:this.state.number_tabs-1,active_tab:'0',tabs_titles:arr,tasks:this.state.tasks.filter(function(elem){return elem.tab!=e.target.id})})
+    this.setState({count:this.state.count,number_tabs:this.state.number_tabs,active_tab:'0',tabs_titles:arr,tasks:this.state.tasks.filter(function(elem){return elem.tab!=e.target.id})})
   }
   editTab(e){
-
+    var arr=this.state.tabs_titles.slice(0);
+    var elem=arr.filter(function(elem){return elem.id==e.target.id})
+    arr=arr.filter(function(elem){return elem.id!=e.target.id})
+    var elem1={title:elem[0].title,id:elem[0].id,editable:true}
+    this.setState({tabs_titles:arr.concat(elem1),count:this.state.count,number_tabs:this.state.number_tabs,active_tab:'0',tasks:this.state.tasks})
   }
+  handleTabChange(e){
+    var arr=this.state.tabs_titles.slice(0);
+    var elem=arr.filter(function(elem){return elem.id==e.target.id})
+    arr=arr.filter(function(elem){return elem.id!=e.target.id})
+    var elem1={title:e.target.value,id:elem[0].id,editable:true}
+    this.setState({tabs_titles:arr.concat(elem1),count:this.state.count,number_tabs:this.state.number_tabs,active_tab:'0',tasks:this.state.tasks})
+  }
+  validateTabChange(e){
+    console.log(e)
+    if(e.key=='Enter'){
+      var arr=this.state.tabs_titles.slice(0);
+      var elem=arr.filter(function(elem){return elem.id==e.target.id})
+      arr=arr.filter(function(elem){return elem.id!=e.target.id})
+      var elem1={title:elem[0].title,id:elem[0].id,editable:false}
+      this.setState({tabs_titles:arr.concat(elem1),count:this.state.count,number_tabs:this.state.number_tabs,active_tab:'0',tasks:this.state.tasks})
+    }
+  }
+
   changeTab(selectedKey){
     this.setState({active_tab:selectedKey})
   }
-  addTodo(){
-    this.setState({number_tabs:this.number_tabs+1})
-  }
   addTab(){
-    this.setState({number_tabs:this.state.number_tabs+1, tabs_titles:this.state.tabs_titles.concat('New Tab'),count:this.state.count+1, tasks:this.state.tasks.concat({title:'New Task',complete:false,tab:this.state.number_tabs+1,editable:false,id:this.state.count})})
+    this.setState({number_tabs:this.state.number_tabs+1, tabs_titles:this.state.tabs_titles.concat({title:'New Tab',id:this.state.number_tabs,editable:false}),count:this.state.count+1, tasks:this.state.tasks.concat({title:'New Task',complete:false,tab:this.state.number_tabs+1,editable:false,id:this.state.count})})
   }
   addTask(){
     this.setState({count:this.state.count+1,tasks:this.state.tasks.concat({title:'New Task',complete:false,tab:this.state.active_tab,id:this.state.count,editable:false})})
@@ -64,7 +85,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Menu numberTabs={this.state.number_tabs} activeTab={this.state.active_tab} changeTab={this.changeTab} nameTabs={this.state.tabs_titles} removeTab={this.removeTab} editTab={this.editTab} addTab={this.addTab}/>
+        <Menu numberTabs={this.state.number_tabs} handleTabChange={this.handleTabChange} activeTab={this.state.active_tab} changeTab={this.changeTab} validateTabChange={this.validateTabChange}nameTabs={this.state.tabs_titles} removeTab={this.removeTab} editTab={this.editTab} addTab={this.addTab}/>
         <ToDoList deleteTask={this.deleteTask}tasks={this.state.tasks} handleChange={this.handleChange} toggleModif={this.toggleModification} activeTab={this.state.active_tab} addTask={this.addTask} toggleTask={this.toggleTask}/>
       </div>
     );
@@ -77,7 +98,12 @@ class Menu extends Component{
   render(){
     var self=this;
     const NavTabs=this.props.nameTabs.map(function(fw, i){
-      return <NavItem eventKey={i.toString()}>{fw}<Glyphicon glyph='pencil' onClick={self.props.editTab}style={{fontSize:'1em',lineHeight:'2em',marginLeft:'3em'}}/><Glyphicon glyph='remove' id={i.toString()} onClick={self.props.removeTab} style={{fontSize:'1em',lineHeight:'2em',marginLeft:'1em'}}/></NavItem>
+      if(!fw.editable){
+      return <NavItem eventKey={fw.id}>{fw.title}<Glyphicon glyph='pencil' id={fw.id} onClick={self.props.editTab}style={{fontSize:'1em',lineHeight:'2em',marginLeft:'3em'}}/><Glyphicon glyph='remove' id={fw.id} onClick={self.props.removeTab} style={{fontSize:'1em',lineHeight:'2em',marginLeft:'1em'}}/></NavItem>
+    }
+      else{
+        return <NavItem><FormControl id={fw.id} onSubmit={self.props.editTab} onKeyDown={self.props.validateTabChange} onChange={self.props.handleTabChange} type='text' defaultValue={fw.title}/></NavItem>
+    }
     })
     return <div>
       <Nav bsStyle="tabs" activeKey={this.props.activeTab} onSelect={this.props.changeTab}>
